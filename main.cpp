7df85bce5071fc0cad4507b7c75968740446446b
Askat Kuzdeuov <askhatkuzdeuov@gmail.com>
2020-01-10 19:01:06 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,53 +1,109 @@
+#include <chrono>
+#include <cmath>
 #include "motorControl.h"
+#define EPS 5 // position error in mV
+#define deg2rad M_PI/180;
+
+
+using namespace std::chrono;
+
 
 int main(){
+    // generate a simple sinusoidal trajectory
+    int size = 120;
+    int posM1[size];
+    int posM2[size];
+    int posM3[size];
+
+    //phase shift, amplitudes and angular velocity
+    double phase = 120*deg2rad;
+    double amp1 = 1800.0;
+    double amp2 = 1800.0;
+    double amp3 = 1800.0;
+    double w = 1;
+
+    // home position
+    int homePosition = 2500;
+
+    //generate motor positions
+    for(int it = 0; it!= size;++it){
+        posM1[it] = -int(amp1*sin(w*it*M_PI/size)) + homePosition;
+        posM2[it] = int(amp2*sin(w*it*M_PI/size)) + homePosition;
+        posM3[it] = int(amp3*sin(w*it*M_PI/size)) + homePosition;
+
+    }
+
+    // define ports and baud rates
     std::string port1("/dev/ttyUSB0");
     speed_t br1 = B19200;
 
     std::string port2("/dev/ttyUSB1");
     speed_t br2 = B19200;
 
-    // 1 Motor
-    Kangaroo motor1(port1, br1);
-    motor1.openSerialPort();
-    motor1.configureSerialPort();
+    // motor IDs
+    char M1 = '1', M2 = '2';
 
-    motor1.startMotor('1');
-    motor1.getPosition('1');
-    /*motor1.setPosition('1');
-    usleep(1000000);
-    motor1.getPosition('1');
-    motor1.homePosition('1');
-    usleep(1000000);
-    motor1.getPosition('1');*/
+    // Connect to Kangaroo controllers
+    // Controller 1
+    Kangaroo kangaroo1(port1, br1);
+    kangaroo1.openSerialPort();
+    kangaroo1.configureSerialPort();
 
-    // 2 Motors
-    Kangaroo motor2(port2, br2);
-    motor2.openSerialPort();
-    motor2.configureSerialPort();
+    // Controller 2
+    Kangaroo kangaroo2(port2, br2);
+    kangaroo2.openSerialPort();
+    kangaroo2.configureSerialPort();
 
-    motor2.startMotor('1');
-    motor2.startMotor('2');
+    // start driver 1 motor 1
+    kangaroo1.startMotor(M1);
+    kangaroo1.getPosition(M1);
 
-    motor2.getPosition('1');
-    motor2.getPosition('2');
+    // start driver 2 motor 1 and 2
+    kangaroo2.startMotor(M1);
+    kangaroo2.startMotor(M2);
 
-    /*motor2.setPosition('1');
-    motor2.setPosition('2');
+    // move to the home position
+    kangaroo1.homePosition(M1);
+    kangaroo2.homePosition(M1);
+    kangaroo2.homePosition(M2);
+    usleep(2000000);
 
-    usleep(1000000);
+    // move to the starting position
+    kangaroo1.setPosition(M1, posM1[0]);
+    kangaroo2.setPosition(M1, posM2[0]);
+    kangaroo2.setPosition(M2, posM3[0]);
+    usleep(4000000);
 
-    motor2.setPosition('1');
-    motor2.setPosition('2');
+    int counter = 1;
+    while(counter < size){
+        std:: cout << "Iteration: " << counter << "-------------" << std::endl;
+        kangaroo2.setPosition(M1, posM2[counter]);
+        kangaroo1.setPosition(M1, posM1[counter]);
+        kangaroo2.setPosition(M2, posM3[counter]);
 
-    motor2.homePosition('1');
-    motor2.homePosition('2');
-    usleep(1000000);
-    motor2.getPosition('1');
-    motor2.getPosition('2');*/
+        usleep(100000);
 
-    motor1.closeSerialPort();
-    motor2.closeSerialPort();
+        kangaroo2.getPosition(M1);
+        kangaroo1.getPosition(M1);
+        kangaroo2.getPosition(M2);
+
+        ++counter;
+    }
+
+    kangaroo1.homePosition(M1);
+    kangaroo2.homePosition(M1);
+    kangaroo2.homePosition(M2);
+
+    usleep(2000000);
+    kangaroo1.getPosition(M1);
+    kangaroo2.getPosition(M1);
+    kangaroo2.getPosition(M2);
+
+
+    kangaroo1.closeSerialPort();
+    kangaroo2.closeSerialPort();
+
+
 
 
     return 0;
